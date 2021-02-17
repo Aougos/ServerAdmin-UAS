9235a581006535225e9f53fe3f07af7fb0831656
Aougos <agustinus132@gmail.com>
2021-02-17 18:17:56 +0000

    patching index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-   echo "hello world";
    $link = mysqli_connect("172.22.0.2","root","root","trucorp");
    if (!$link) {
       echo "tidak bisa konek ke MySql" . PHP_EOL;
@@ -7,17 +6,20 @@
    }
    $db = "SELECT * FROM users";
    $result = $link->query($db);
+   $user = 0;
 
    if ($result->num_rows > 0) {
+      echo "Table User Database trucorp-web-2.0" . PHP_EOL;
       echo "<table><tr><th>ID</th><th>Nama</th><th>Kantor</th></tr>";
       while($row = $result->fetch_assoc()) {
-//	 echo "Table User Database trucorp-2" . PHP_EOL;
-         echo "<tr><td>id: " . $row["ID"]. "</td><td>" . $row["Name"]. "</td><td>" . $row["Kantor"]. "</td></tr>" . PHP_EOL;
+	 $user = $user + 1;
+         echo "<tr><td>" . $row["ID"]. "</td><td>" . $row["Nama"]. "</td><td>" . $row["Kantor"]. "</td></tr>" . PHP_EOL;
       }
       echo "</table>";
    }else {
       echo "Database Kosong" . PHP_EOL;
    }
+   echo "Jumlah user: " . $link . PHP_EOL;
    echo "Host Information: " . mysqli_get_host_info($link) . PHP_EOL;
    mysqli_close($Link);
 ?>
